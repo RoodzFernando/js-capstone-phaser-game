e6aeaa25bf6de9c19392c0b491a454725cb16c41
RoodzFernando <50186903+RoodzFernando@users.noreply.github.com>
2020-07-05 15:00:50 -0400

    Add more config files to add phaser

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-const messageElt = document.createElement('h1');
-messageElt.textContent = 'Welcome from Webpack!';
-document.body.appendChild(messageElt);
+import 'phaser';
+import config from './Config/config';
+import GameScene from './Scenes/GameScene';
+
+class Game extends Phaser.Game {
+  constructor() {
+    super(config);
+    this.scene.add('Game', GameScene);
+    this.scene.start('Game');
+  }
+}
+
+window.game = new Game();
